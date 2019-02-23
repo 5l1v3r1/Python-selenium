69898b4872bd22f6297480aa8827c1cf5cfe4733
Илья <ilya-31071992@mail.ru>
2019-02-23 23:57:45 +0300

    Оптимизировал ввод данных в формы

--- Model/group.py
+++ Model/group.py
@@ -1,5 +1,5 @@
 class group:
-    def __init__(app,name,header,footer):
+    def __init__(app, name=None, header=None, footer=None):
         app.name = name
         app.header = header
         app.footer = footer
--- Test/group/test_edit_group.py
+++ Test/group/test_edit_group.py
@@ -1,6 +1,6 @@
 from Model.group import group
 
-def test_edit_first_group(app):
+def test_edit_first_group_name(app):
     app.session.Login(username="admin", password="secret")
-    app.Group.edit_group(group(name="namsadasdsae2", footer="footssadadasdasasder2", header="headercxcxczxcxcxzcc"))
+    app.Group.edit_group(group(name="New Group"))
     app.session.Logout()
