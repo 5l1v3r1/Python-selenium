98917db6dfef7b7871bb703a71eb4af03fdc0009
Илья <ilya-31071992@mail.ru>
2019-03-03 18:41:46 +0300

    Урок 20 улучшил проверку удаления групп

--- Model/group.py
+++ Model/group.py
@@ -4,3 +4,11 @@ class Group:
         app.header = header
         app.footer = footer
         app.id = id
+
+
+    def __repr__(self):
+        return "%s: %s" % (self.id, self.name)
+
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
