72620ef2324a2a945272e87ce7991efa1b686703
Илья <ilya-31071992@mail.ru>
2019-03-25 00:16:34 +0300

    При использовании decoders происходила ошибка. Отключил декодер, теперь запросы к бД работают

---
+++ Fixture/db.py
@@ -0,0 +1,64 @@
+import pymysql.cursors
+from Model.group import Group
+from Model.address import Address
+class DbFixture:
+    def __init__(self, host, name, user, password):
+        self.user = user
+        self.name = name
+        self.host = host
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor.fetchall():
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), header=header, footer=footer, name=name))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, email, mobile from addressbook where deprecated ='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, email, mobile) = row
+                list.append(Address(id=str(id), firstname=firstname, lastname=lastname, address=address, mobile=mobile, email=email))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list_with_merged_emails_and_phones(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3 from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3) = row
+                list.append(
+                    Address(id=str(id), firstname=firstname, lastname=lastname, address=address, all_phones_from_home_page=home + mobile + work + phone2, all_emails_from_home_page=email + email2 + email3))
+        finally:
+            cursor.close()
+        return list
+
+
+    def get_list_of_groups_names_and_ids(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name from group_list")
+            for row in cursor:
+                (id, name) = row
+                list.append(Group(id=str(id), name=name))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
---
+++ check_db_connection.py
@@ -0,0 +1,40 @@
+from Fixture.orm import ORMFixture
+from Model.group import Group
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+try:
+    contacts = db.get_contacts_not_in_group(Group(id="263"))
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
+finally:
+    pass #db.destroy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#from Fixture.db import DbFixture
+
+#db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+#try:
+    #contacts = db.get_contact_list()
+    #for contact in contacts:
+        #print(contact)
+    #print(len(contacts))
+#finally:
+    #db.destroy()
